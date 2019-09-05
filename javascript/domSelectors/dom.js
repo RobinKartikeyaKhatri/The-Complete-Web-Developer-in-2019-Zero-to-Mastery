41966738342e66e843c4f6a40a49035e17a40733
robinkartik <kartikrobin@gmail.com>
2019-09-05 15:11:34 +0530

    Learned about DOM event listeners

--- javascript/domSelectors/dom.js
+++ javascript/domSelectors/dom.js
@@ -1,41 +1,30 @@
-console.log(document.getElementsByTagName("p"));
+var button = document.querySelector("button");
+var input = document.querySelector("input");
+var ul = document.querySelector("ul");
 
-console.log(document.getElementsByClassName("second"));
+function inputLength() {
+    return input.value.length;
+}
 
-console.log(document.getElementById("first"));
+function createListElement() {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+}
 
-console.log(document.querySelector("p"));
+function addListAfterClick() {   
+    if (inputLength() > 0) {
+        createListElement();
+    }
+}
 
-console.log(document.querySelectorAll("li"));
+function addListAfterKepress(event) {
+    if (inputLength() > 0 && event.keyCode === 13) {
+        createListElement();
+    }
+}
 
-var firstLi = document.querySelector("li");
-console.log(firstLi.getAttribute("random"));
-console.log(firstLi.getAttribute("placeholder"));
+button.addEventListener("click", addListAfterClick);
 
-var secondLi = document.querySelectorAll("li")[1];
-console.log(secondLi);
-secondLi.setAttribute("value", "half kg");
-
-// var h1 = document.querySelector("h1");
-// h1.style.color = "red";
-// h1.style.backgroundColor = "yellow";
-// h1.style.fontSize = "200px";
-
-var allLis = document.querySelectorAll("li");
-allLis.forEach(function (li) {
-    li.style.color = "purple";
-});
-
-var h1 = document.querySelector("h1");
-h1.className = "coolTitle";
-
-var myFirstLi = document.querySelector("li");
-console.log(myFirstLi.classList);
-
-var myAllListItems = document.querySelectorAll("li");
-myAllListItems.forEach(function (li) {
-    li.classList.toggle("done");
-});
-
-var firstP = document.querySelector("p");
-console.log(firstP.parentElement);
+input.addEventListener("keypress", addListAfterKepress);
