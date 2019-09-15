ed49ceffdf82474b55e5ad78dbb771e70955b01b
robinkartik <kartikrobin@gmail.com>
2019-09-15 14:17:25 +0530

    Learned about includes() and Exponent in ES7

---
+++ advancedJavaScript/es7/script.js
@@ -0,0 +1,10 @@
+// includes
+console.log("Helloooooo".includes("o"));
+
+const pets = ["cat", "dog", "bat"];
+console.log(pets.includes("bat"));
+console.log(pets.includes("bird"));
+
+// Exponent
+const square = (x) => x**2;
+console.log(square(2));
