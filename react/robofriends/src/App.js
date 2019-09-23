3f3cff908cc3860668766eece43ae6dca9066d25
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2019-09-23 16:58:03 +0530

    Learned how to create a Component and how to use it

--- react/robofriends/src/App.js
+++ react/robofriends/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+1. import React from 'react';
+2. import logo from './logo.svg';
+3. import './App.css';
+4.  
+5. class App extends Component {
+6.   render() {
+7.     return (
+8.       <div className="App">
+9.         <header className="App-header">
+10.           <img src={logo} className="App-logo" alt="logo" />
+11.           <p>
+12.             Edit <code>src/App.js</code> and save to reload.
+13.           </p>
+14.           <a
+15.             className="App-link"
+16.             href="https://reactjs.org"
+17.             target="_blank"
+18.             rel="noopener noreferrer"
+19.           >
+20.             Learn React
+21.           </a>
+22.         </header>
+23.       </div>
+24.     );
+25.   }
+26. }
+27.  
+28. export default App;
